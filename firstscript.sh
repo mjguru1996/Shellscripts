195b26c24caab7d3748003862335045ee1cf7682
Guruprasad Kannan <130130177+mjguru1996@users.noreply.github.com>
2025-08-24 19:20:18 +0530

    updatedCommit 4

--- firstscript.sh
+++ firstscript.sh
@@ -2,6 +2,9 @@
 set -x # Setting up debug mode
 df -h #To list the volumes in VM
 free -g
-nproc # To See the Running Process in VM
+nproc # To See the Number of processor  in VM
 top #To Verify all service parameters running
+ps -ef #To List all the Process running in the VM
+ps -ef | grep "amazon" # Grep Command is Used to filter the output and Pipe Command is used to send the Output from one command to another command.:wq!
+
 
