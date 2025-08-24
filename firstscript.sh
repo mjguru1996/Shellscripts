13366c7400e2439b4e1b14f2356d28ea9af56fb4
Guruprasad Kannan <130130177+mjguru1996@users.noreply.github.com>
2025-08-24 19:34:58 +0530

    updatedCommit 5

--- firstscript.sh
+++ firstscript.sh
@@ -5,8 +5,8 @@ echo "********** To list the Free memory space in VM ******"
 free -g
 echo "********** To see the no of processor in VM ******"
 nproc # To See the Number of processor  in VM
-echo "********** To verify all service parameters in VM ******"
-top
+# echo "********** To verify all service parameters in VM ******"
+# top
 echo "********** To List all the Process running in the VM ******"
 ps -ef
 echo "********** Grep Command is Used to filter the output and Pipe Command is used to send the Output from one command to another command ******"
