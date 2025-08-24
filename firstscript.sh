5df60c70c81584f161ed868144a3e1de4bc84e84
Guruprasad Kannan <130130177+mjguru1996@users.noreply.github.com>
2025-08-24 19:33:19 +0530

    updatedCommit 5

--- firstscript.sh
+++ firstscript.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-echo "**********SETTING UP DEBUG MODE******"
-# set -x # Setting up debug mode
 echo "********** To list the volumes in VM ******"
 df -h
 echo "********** To list the Free memory space in VM ******"
 free -g
+echo "********** To see the no of processor in VM ******"
 nproc # To See the Number of processor  in VM
-top #To Verify all service parameters running
-ps -ef #To List all the Process running in the VM
-ps -ef | grep "amazon" # Grep Command is Used to filter the output and Pipe Command is used to send the Output from one command to another command.:wq!
+echo "********** To verify all service parameters in VM ******"
+top
+echo "********** To List all the Process running in the VM ******"
+ps -ef
+echo "********** Grep Command is Used to filter the output and Pipe Command is used to send the Output from one command to another command ******"
+ps -ef | grep "amazon"
 
 
